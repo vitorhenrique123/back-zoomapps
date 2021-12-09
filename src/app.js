dcfb959a569801632ce47a9f10ddafff547204bd
vitorhenrique123 <vitor@vitorhenrique.me>
2021-12-09 16:45:51 -0300

    update
    sd

--- src/app.js
+++ src/app.js
@@ -11,18 +11,30 @@ const classes = require('./routes/classes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true, 
+    methods: 'POST,GET,PUT,OPTIONS,DELETE'
+}));
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.header("Access-Control-Allow-Methods", "GET, POST","PUT", "OPTIONS");
+    next();
+});
 
 //app.use(index);
 app.post('/login', user.loginUser);
 app.post('/register', user.registernUser);
 app.post('/creategroup', classes.createGroup);
 app.post('/getgroupmembers', classes.listGroup);
+app.post('/listgroupmemebers', classes.listGroupMembers);
+listGroupMembers
 
 module.exports = app;
--- src/routes/classes.js
+++ src/routes/classes.js
@@ -39,15 +39,17 @@ const createGroup = (request, response) => {
         });
         res.on("end", function () {
             var response = JSON.parse(chunks);
-            db.pool.query('INSERT INTO public.grupos( id_usuario, id_grupo, nome_grupo) VALUES (?, ?)', [jsopn.admin_id,response.id, response.name], (error, results) => {
-                response.status(200).send({
-                    reponse: 'created'
-                });
-            })
-
+            console.log(response);
+            db.pool.query('SELECT * FROM  public.usuarios  WHERE email = $1', [jsopn.admin_email], (error, results) => {
+                db.pool.query('INSERT INTO public.grupos( id_usuario, id_grupo, name) VALUES ($1, $2, $3)', [results.rows[0].id,response.id, response.name], (error2, results2) => {
+                    console.log("teste");
+                })
+            });
         });
       })
-
+      response.status(200).send({
+        reponse: 'created'
+    });
       
       req.on('error', error => {
         console.error(error)
@@ -64,9 +66,45 @@ const listGroup = (request, response) => {
             data: results.rows
         });
     })
+}
+
+const listGroupMembers = (request, response) => {
+    const jsopn = request.body;
+    const options = {
+        hostname: `api.zoom.us`, 
+        path: `/v2/groups/${group_id}/members`,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Zoom-api-Jwt-Request',
+            'content-type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    };
+    var postData = JSON.stringify(
+        {
+            "name": jsopn.name
+        }
+    );
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+      
+        var chunks = '';
+        res.on("data", function (chunk) {
+            chunks += chunk;
+        });
+        res.on("end", function () {
+            var response = JSON.parse(chunks);
+            response.status(200).send({
+                reponse: response
+            });
+
+        });
+      })
+
 
 }
 module.exports = {
     createGroup,
-    listGroup
+    listGroup,
+    listGroupMembers
 }
